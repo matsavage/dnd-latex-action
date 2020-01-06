9dd89cd93840267fe403d8e38432f9045bdff5db
Mathew Savage <mathew@mathewsavage.com>
2020-01-06 10:43:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,10 +21,12 @@ if [ -n "$working_directory" ]; then
 fi
 
 tlmgr conf
-tlmgr list --only-installed
 
 echo "$(kpsewhich -var-value TEXMFLOCAL)/tex/latex/local/"
 ls -la "$(kpsewhich -var-value TEXMFLOCAL)/tex/latex/local/"
 ls -la "$(kpsewhich -var-value TEXMFLOCAL)/tex/latex/local/dnd"
+
+echo "$(kpsewhich -var-value TEXMFHOME)"
+ls -la "$(kpsewhich -var-value TEXMFHOME)
 
 "$compiler" $args "$root_file"
